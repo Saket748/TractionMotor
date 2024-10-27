a20cf35a413cde5a7da1d9a62648e2bb864f9689
Saket yadav <127470855+Saket748@users.noreply.github.com>
2024-10-27 14:31:20 +0530

    Update MotorTable.java

--- src/com/Traction/motor/MotorTable.java
+++ src/com/Traction/motor/MotorTable.java
@@ -1,4 +1,71 @@
 package com.Traction.motor;
 
 public class MotorTable {
+    private int SerialNo;
+    private String MotorNumber;
+    private String ProjectName;
+    private String SupplyDate;
+
+    public MotorTable(int serialNo, String motorNumber, String projectName, String supplyDate) {
+        super();
+        SerialNo = serialNo;
+        MotorNumber = motorNumber;
+        ProjectName = projectName;
+        SupplyDate = supplyDate;
+    }
+
+    public MotorTable(String motorNumber, String projectName, String supplyDate) {
+        super();
+        MotorNumber = motorNumber;
+        ProjectName = projectName;
+        SupplyDate = supplyDate;
+    }
+
+    public MotorTable() {
+        super();
+    }
+
+    public int getSerialNo() {
+        return SerialNo;
+    }
+
+    public void setSerialNo(int serialNo) {
+        SerialNo = serialNo;
+    }
+
+    public String getMotorNumber() {
+        return MotorNumber;
+    }
+
+    public void setMotorNumber(String motorNumber) {
+        MotorNumber = motorNumber;
+    }
+
+    public String getProjectName() {
+        return ProjectName;
+    }
+
+    public void setProjectName(String projectName) {
+        ProjectName = projectName;
+    }
+
+    public String getSupplyDate() {
+        return SupplyDate;
+    }
+
+    public void setSupplyDate(String supplyDate) {
+        SupplyDate = supplyDate;
+    }
+
+    //Display method
+
+    @Override
+    public String toString() {
+        return "MotorTable{" +
+                "SerialNo=" + SerialNo +
+                ", MotorNumber='" + MotorNumber + '\'' +
+                ", ProjectName='" + ProjectName + '\'' +
+                ", SupplyDate='" + SupplyDate + '\'' +
+                '}';
+    }
 }
