cc3d300993839dd0bff68a43892a735310cd1c18
Saket yadav <127470855+Saket748@users.noreply.github.com>
2024-10-27 14:26:49 +0530

    Update ConnectionProvider.java

--- src/com/Traction/motor/ConnectionProvider.java
+++ src/com/Traction/motor/ConnectionProvider.java
@@ -1,4 +1,24 @@
 package com.Traction.motor;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionProvider {
+    static Connection con;
+    public static Connection createC()throws IOException {
+        try{
+            //loading the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //create connection
+            String url = "jdbc:mysql://localhost:3306/VandeBharatTractionMotor?useSSL=false";
+            String user = "Saket-yadav";
+            String password = "example-password";
+            con = DriverManager.getConnection(url,user,password);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
